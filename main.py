41362daae4abecdc321fa8e7bf1a80c23f80e3a8
Izundu Clara Chinecherem <ci.chinecherem@stu.unizik.edu.ng>
2024-11-29 17:10:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+print ("Generation Identifier")
+generation= int(input("What year were you born?"))
+if generation >= 1883 and generation <= 1900:
+  print()
+  print("You are part of the Lost Generation")
+elif generation >= 1901 and generation <= 1927:
+  print()
+  print("You are part of the Greatest Generation")
+elif generation >= 1928 and generation <= 1945:
+  print()
+  print("You are part of the Silent Generation")
+elif generation >= 1946 and generation <= 1964:
+  print()
+  print("You are part of the Baby Boomers Generation")
+elif generation  >= 1965 and generation <= 1980:
+  print()
+  print("You are part of Generation X Generation")
+elif generation >= 1981 and generation <= 1996:
+  print()
+  print("You are part of the Millennials Generation")
+elif generation >= 1997 and generation <= 2012:
+  print()
+  print("You are a Gen Z baddie")
+elif generation >= 2013 and generation <= 2024:
+  print()
+  print("You are part of the Generation Alpha")
+else:
+  print()
+  print("You do not exist!")
